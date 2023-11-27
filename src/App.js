90b64f27e597853b768b6ae9882f4b1b1e7e3a29
Carlos Barrero Martínez <barrerot@gmail.com>
2023-11-27 20:06:54 +0100

    AdvertsPage- mocked

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import AdvertsPage from "./pages/adverts/AdvertsPage";
+
 
 function App() {
   return (
     <div className="App">
+      <AdvertsPage></AdvertsPage>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
+        <h1>
           Learn React
-        </a>
+        </h1>
       </header>
     </div>
   );
